0f9eeabcbcfa6ff43d08cda323c8d0ac410d858d
óz <ozmursleen@users.noreply.github.com>
2017-05-02 16:48:19 +0500

    Updated autoid method

--- oz-notifier/src/components/oz-notifier-service.js
+++ oz-notifier/src/components/oz-notifier-service.js
@@ -52,14 +52,17 @@ function OZNotificationService($timeout) {
         return that.notifications;
     };
 
-    that.autoId = function(){
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-        for( var i=0; i < 5; i++ )
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-        return text;
+    //taken from: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+    that.autoId = function() { // Public Domain/MIT
+        var d = new Date().getTime();
+        if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+            d += performance.now(); //use high-precision timer if available
+        }
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
     };
 }
 
